87046e7c15d1ac81ea4f3529fbc634af1554c00c
Ayush Jain <ayushjain2205@gmail.com>
2023-10-24 16:09:28 +0530

    Init basic ShadCN ui and routes folders

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <Button>Dashborad</Button>
+    </div>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <p>Landing Page - unprotected</p>
+      <Button>Hello</Button>
+    </div>
+  );
+}
