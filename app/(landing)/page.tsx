3c3fb1b3cb43f504b1185a365fa549535bc2968d
Ayush Jain <ayushjain2205@gmail.com>
2023-11-01 23:53:36 +0530

    Check landing page render

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function LandingPage() {
   return (
     <div>
-      <p>Landing Page - unprotected</p>
-      <Button>Hello</Button>
+      <p>SHow landing page</p>
     </div>
   );
 }
